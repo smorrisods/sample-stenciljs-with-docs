02d6398ebdc47bdebcd351df0018d24103df7d7d
Scott Morris <scott.morris@ontario.ca>
2025-05-12 13:34:52 -0400

    fix(tsconfig): ensure project works properly with ES2022 support 🚀
    - Set `"target": "ES2022"` in `tsconfig.json`.
    - Enabled `"useDefineForClassFields": true` to ensure proper handling of Stencil's getter/setter wiring.

--- packages/app-nextjs/src/app/client-test/page.tsx
+++ packages/app-nextjs/src/app/client-test/page.tsx
@@ -1,34 +1,14 @@
-// 'use client';
-
-// import { MyComponent } from 'react-library';
-
-// export default function ClientTestPage() {
-// 	return (
-// 		<main>
-// 			<h2>Client-Only Version</h2>
-// 			<MyComponent first="Bart" last="Simpson"></MyComponent>
-// 		</main>
-// 	);
-// }
-
 "use client";
 
-// import { useEffect } from "react";
-// import { defineCustomElements } from "stencil-library/loader";
+import { useEffect } from "react";
 import { MyComponent } from "react-library";
 
-// function WebComponentsInitializer() {
-//   useEffect(() => {
-//     if (typeof window !== "undefined") {
-//       defineCustomElements(window);
-//     }
-//   }, []);
-
-//   return null; // This component doesn't render anything
-// }
-
 export default function ClientTestPage() {
-  //   WebComponentsInitializer();
+  useEffect(() => {
+    console.log("🧪 MyComponent (raw):", MyComponent);
+    console.log("🧪 MyComponent (string):", MyComponent?.toString?.());
+  }, []);
+
   return (
     <main>
       <h2>Client-Only Version</h2>
